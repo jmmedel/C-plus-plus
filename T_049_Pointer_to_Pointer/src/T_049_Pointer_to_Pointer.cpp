bfc98ada8c5b85ec4cf1737dc0e4ce7bf2ff4dbd
anodaw98 <jm.kagaya@gmail.com>
2018-03-06 04:01:14 -0800

    Not_done

---
+++ T_049_Pointer_to_Pointer/src/T_049_Pointer_to_Pointer.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : T_049_Pointer_to_Pointer.cpp
+// Author      : kagaya
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+
+/*
+ *C++ Pointer to Pointer (Multiple Indirection)
+ *
+ * */
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
